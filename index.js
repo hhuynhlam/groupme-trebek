b0acf30c581215ba9f381ee493cb6706e0f5f786
Hai Huynhlam <hhuynhlam@gmail.com>
2016-12-07 13:49:37 -0800

    wip

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
-var http, director, cool, bot, router, server, port;
-
-http        = require('http');
-director    = require('director');
-cool        = require('cool-ascii-faces');
-bot         = require('./bot.js');
-
-router = new director.http.Router({
-  '/' : {
-    post: bot.respond,
-    get: ping
-  }
-});
-
-server = http.createServer(function (req, res) {
-  req.chunks = [];
-  req.on('data', function (chunk) {
-    req.chunks.push(chunk.toString());
-  });
-
-  router.dispatch(req, res, function(err) {
-    res.writeHead(err.status, {"Content-Type": "text/plain"});
-    res.end(err.message);
-  });
-});
-
-port = Number(process.env.PORT || 5000);
-server.listen(port);
+const http = require('http');
+const director = require('director');
+const bot = require('./bot.js');
 
 function ping() {
   this.res.writeHead(200);
   this.res.end("Hey, I'm Cool Guy.");
 }
+
+const router = new director.http.Router({
+  '/': {
+    post: bot.respond,
+    get: ping,
+  },
+});
+
+const port = Number(process.env.PORT || 3000);
+const server = http.createServer((req, res) => {
+  req.chunks = [];
+
+  req.on('data', (chunk) => {
+    req.chunks.push(chunk.toString());
+  });
+
+  router.dispatch(req, res, (err) => {
+    res.writeHead(err.status, { 'Content-Type': 'text/plain' });
+    res.end(err.message);
+  });
+});
+
+server.listen(port);
